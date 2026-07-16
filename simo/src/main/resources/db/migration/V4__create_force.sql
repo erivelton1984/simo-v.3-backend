37a96359a2c30048c5e83ef97377f691d51d4d29
Erivelton Ribeiro <erivelton84ribeiro@gmail.com>
2026-07-16 11:03:07 -0300

    Subindo migratioins.

---
+++ simo/src/main/resources/db/migration/V4__create_force.sql
@@ -0,0 +1,16 @@
+CREATE TABLE forces
+(
+    id BIGSERIAL PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL UNIQUE,
+
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+
+    created_at TIMESTAMP,
+
+    updated_at TIMESTAMP,
+
+    created_by VARCHAR(100),
+
+    updated_by VARCHAR(100)
+);
